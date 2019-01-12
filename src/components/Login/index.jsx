7fdbe7425309b5d8cd2333767196059aaf62f0c1
Vladislav Klochkov <kvladislavo2013@gmail.com>
2019-01-12 16:25:49 +0300

    Styled login page.

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -3,12 +3,15 @@ import { Redirect } from 'react-router-dom';
 
 import { isAuthenticated } from '../../utils/authUtil';
 
+import login_icon from '../../images/login_icon.png';
+import './styles.css';
+
 const Login = ({ authToken, login, location }) => isAuthenticated() ?
   <Redirect to={(location.state && location.state.from) || '/' } /> :
   (
-    <div>
-      <p>LOGIN</p>
-      <button onClick={login}>Log in</button>
+    <div className="login-wrapper">
+      <img src={login_icon} alt="login-icon"/>
+      <button onClick={login}>Let me in</button>
     </div>
   );
 
